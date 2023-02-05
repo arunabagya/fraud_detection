b5cd304965f12b999ed9685473e3d28175f005d5
arunabagya <arunabagya@gmail.com>
2023-02-05 17:25:11 +0530

    update file

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,11 @@
 print("adding all the data processing code related fraud detection")
+a=5
+b=7
+sum(a,b):
+   c=a+b
+   return c
+print("sum function")
+def multiplication(a,b);
+  d=a*b
+  return b
+  print("multiplication function")
